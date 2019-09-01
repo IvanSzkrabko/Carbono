b79d21de6eee78b9b2bdb4d6dc644b44fdf8de02
LucasGaleano <l.a.galeano2@gmail.com>
2019-09-01 15:50:45 -0300

    agrego presentacion

--- app/src/main/java/com/starkind/ivn/carbono/MainActivity.java
+++ app/src/main/java/com/starkind/ivn/carbono/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
